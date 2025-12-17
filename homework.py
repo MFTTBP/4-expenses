bb820f6f688e9870b844a4cc4e8b9740ba3094c8
MFTTBP <rebro_leorika@bk.ru>
2025-12-17 21:50:22 +0300

    ну хотя бы сейчас

--- homework.py
+++ homework.py
@@ -6,19 +6,21 @@ b = a.split(" ")
 c = False if b[0].isdigit() else True
 
 if len(b) != 2 and len(b) != 4:
-    print("Некорректный формат суммы1")
+    print("Некорректный формат суммы")
+elif not b[0]:
+    print("Некорректный формат суммы")
 elif str(b[0]).startswith("00"):
-    print("Некорректный формат суммы2")
+    print("Некорректный формат суммы")
 elif b[1] != "руб":
-    print("Некорректный формат суммы3")
+    print("Некорректный формат суммы")
 elif len(b) == 4 and b[3] != "коп":
-    print("Некорректный формат суммы4")
+    print("Некорректный формат суммы")
 elif c:
-    print("Некорректный формат суммы5")
+    print("Некорректный формат суммы")
 elif len(b) == 4 and b[2].isdigit() is False:
-    print("Некорректный формат суммы6")
-elif len(b) == 4 and int(b[2]) > 99 and int(b[2]) < 0:
-    print("Некорректный формат суммы7")
+    print("Некорректный формат суммы")
+elif len(b) == 4 and int(b[2]) > 99 or int(b[2]) < 0:
+    print("Некорректный формат суммы")
 elif len(b) == 2:
     print(f"{b[0]}.00 ₽")
 else:
