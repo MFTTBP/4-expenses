684c2e7f7039fe0e601b2dffc102f9b865ec9887
MFTTBP <rebro_leorika@bk.ru>
2025-12-17 22:11:50 +0300

    чел успокойся

--- homework.py
+++ homework.py
@@ -3,10 +3,9 @@
 #"""
 a = input("").lower().strip()
 b = a.strip().split()
-c = False if b[0].isdigit() else True
 
 
-if len(b) != 2 and len(b) != 4:
+if len(b) < 2 or len(b) > 4:
     print("Некорректный формат суммы")
 elif not b[0]:
     print("Некорректный формат суммы")
@@ -16,12 +15,11 @@ elif b[1] != "руб":
     print("Некорректный формат суммы")
 elif len(b) == 4 and b[3] != "коп":
     print("Некорректный формат суммы")
-elif c:
+elif len(b) == 2 and not b[0].isdigit():
     print("Некорректный формат суммы")
 elif len(b) == 4 and not b[2].isdigit():
     print("Некорректный формат суммы")
 elif len(b) == 4 and int(b[2]) > 99:
-
     print("Некорректный формат суммы")
 elif len(b) == 2:
     print(f"{b[0]}.00 ₽")
