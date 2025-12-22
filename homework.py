cb678ab4fa6409b06316e86572133c6a598fefc7
MFTTBP <rebro_leorika@bk.ru>
2025-12-22 15:28:49 +0300

    дуухушка

--- homework.py
+++ homework.py
@@ -1,27 +1,17 @@
 """
 #Мое дз
 #"""
-a = input("").lower().strip()
-b = a.strip().split()
+a = [
+    "Добавить расход",
+    "Показать все расходы",
+    "Показать сумму и средний расход",
+    "Удалить расход по номеру",
+]
 
 
-if len(b) < 2 or len(b) > 4:
-    print("Некорректный формат суммы")
-elif not b[0]:
-    print("Некорректный формат суммы")
-elif str(b[0]).startswith("00"):
-    print("Некорректный формат суммы")
-elif b[1] != "руб":
-    print("Некорректный формат суммы")
-elif len(b) == 4 and b[3] != "коп":
-    print("Некорректный формат суммы")
-elif len(b) == 2 and not b[0].isdigit():
-    print("Некорректный формат суммы")
-elif len(b) == 4 and not b[2].isdigit():
-    print("Некорректный формат суммы")
-elif len(b) == 4 and int(b[2]) > 99:
-    print("Некорректный формат суммы")
-elif len(b) == 2:
-    print(f"{b[0]}.00 ₽")
-else:
-    print(f"{b[0]}.{b[2].zfill(2)} ₽")
+while True:
+    for i in a:
+        print(i)
+    user = input("").capitalize()
+    if user == "Выход":
+        break
