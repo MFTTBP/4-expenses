a228e5262854e5c980e5a7330911527640185cec
MFTTBP <rebro_leorika@bk.ru>
2026-01-16 19:04:46 +0300

    my firs try

--- homework.py
+++ homework.py
@@ -2,17 +2,45 @@
 #Мое дз
 #"""
 menu = [
-    "Добавить расход",
-    "Показать все расходы",
-    "Показать сумму и средний расход",
-    "Удалить расход по номеру",
+    "1 - Добавить расход",
+    "2 - Показать все расходы",
+    "3 - Показать сумму и средний расход",
+    "4 - Удалить расход по номеру",
     "Выход"
 ]
+expenses = []
 
+def add_expense(value: float):
+    expenses.append(value)
+    
+
+def summa():
+    s = sum(expenses)
+    return s
+
+def srednee():
+    s = sum(expenses) / len(expenses)
+    return s
+
+def delete():
+    del expenses[del_us]
+    return expenses
 
 while True:
     for i in menu:
         print(i)
     user = input("").capitalize()
+    if user == "1":
+        user_exp = float(input(""))
+        add = add_expense(user_exp)
+    elif user == "2":
+        print(expenses)
+    elif user == "3":
+        print(f"Сумма: {summa()}")
+        print(f"Среднее значение: {srednee()}")
+    elif user == "4":
+        del_us = int(input(""))
+        dele = delete()
     if user == "Выход":
         break
+
